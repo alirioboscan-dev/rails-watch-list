f89de91b9ecad63fb7e3475a50e6e9ef5bedd8f3
Alirio Boscan <alirioboscan@outlook.com>
2026-05-09 15:45:33 +0000

    Embed new bookmark form directly on list show page

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,10 +1,4 @@
 class BookmarksController < ApplicationController
-  def new
-    @list = List.find(params[:list_id])
-    @bookmark = Bookmark.new
-    @movies = Movie.all
-  end
-
   def create
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [:new, :create]
+    resources :bookmarks, only: [:create]
   end
 
   resources :bookmarks, only: [:destroy]
