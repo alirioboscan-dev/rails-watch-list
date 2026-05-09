0d2e2b932c84032717d7367ff868a1d375b1098e
Alirio Boscan <alirioboscan@outlook.com>
2026-05-09 15:35:17 +0000

    Add Bookmarks controller with new, create and destroy actions

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,32 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+    @movies = Movie.all
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list), notice: "Movie added to your list!"
+    else
+      @movies = Movie.all
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @list = @bookmark.list
+    @bookmark.destroy
+    redirect_to list_path(@list), notice: "Bookmark removed."
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id, :comment)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "bookmarks/new"
+  get "bookmarks/create"
+  get "bookmarks/destroy"
   get "lists/index"
   get "lists/show"
   get "lists/new"
@@ -14,5 +17,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :lists, only: [:index, :show, :new, :create]
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: [:destroy]
 end
